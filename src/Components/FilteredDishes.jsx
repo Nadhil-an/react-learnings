f31e4e6db3d495940d0a22eccab873d45d3366af
Nadhil-an <nadhil.edu@gmail.com>
2025-08-13 17:15:08 +0530

    category components

--- src/Components/FilteredDishes.jsx
+++ src/Components/FilteredDishes.jsx
@@ -1,4 +1,10 @@
-function FilteredDishes(){
+function FilteredDishes(props){
+
+    let dishCategory = props.dishcategories.map((item)=>{
+        return(
+            <li>{item.strCategory}</li>
+        )
+    })
     return(
 
         <div className="filtered-dishes">
@@ -7,7 +13,12 @@ function FilteredDishes(){
                     <h2>Choose your Dishes</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis neque exercitationem laborum temporibus magnam maiores a assumenda incidunt!
                     </p>
+                </div>
 
+                <div className="filtered-dishes">
+                    <ul>
+                        {dishCategory}
+                    </ul>
                 </div>
             </div>
              
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { use, useEffect, useState } from "react"
 import Hero from "./Hero.js"
 import SpecialDishes from "./SpecialDishes.jsx"
 import FilteredDishes from "./FilteredDishes.jsx"
@@ -6,6 +6,7 @@ import FilteredDishes from "./FilteredDishes.jsx"
 
 function Menu(){
     let [menu,setMenu] = useState([])
+    let [category,setCategory] = useState([])
 
     async function getFoodMenu(){
         const API_URL = "https://www.themealdb.com/api/json/v1/1/search.php?f=c"
@@ -18,10 +19,8 @@ function Menu(){
         const API_URL = "https://www.themealdb.com/api/json/v1/1/categories.php"
         let response = await fetch(API_URL)
         let categorydata = await response.json()
-        console.log("category",categorydata.categories);
+        setCategory(categorydata.categories)
     }
-
-    console.log("all meals:",menu);
 
     useEffect(()=>{
         getFoodMenu()
@@ -35,7 +34,7 @@ function Menu(){
         
         <Hero />
         <SpecialDishes specialDishes={menu} />
-        <FilteredDishes />
+        <FilteredDishes dishcategories={category}/>
 
 
         </>
