a6e4bcef0df4ece72aa8132632d748c903a3ba83
Nadhil-an <nadhil.edu@gmail.com>
2025-08-21 14:37:52 +0530

    popup functionality completed

--- src/Components/Popup.jsx
+++ src/Components/Popup.jsx
@@ -5,8 +5,21 @@ function Popup({setShowPop,allDishes,popDishes}) {
     return dish.strMeal === popDishes
   }).map((item)=>{
     return (
-      <div className="popup-content-inner">
-        <h3>{item.strMeal}</h3>
+      <div className="popup-content-data">
+        <div className="popup-header">
+          <img src={item.strMealThumb} />
+          <h5 className='popup-header-category'>{item.strCategory}</h5>
+        </div>
+        <h2>{item.strMeal}</h2>
+        <p>{item.strInstructions}</p>
+        <ul className="dish-ingredients flex">
+          <li>{item.strIngredient1}</li>
+          <li>{item.strIngredient2}</li>
+          <li>{item.strIngredient3}</li>
+          <li>{item.strIngredient4}</li>
+
+        </ul>
+        
       </div>
     )
   })
